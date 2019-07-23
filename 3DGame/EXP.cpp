372a77930f93a228f8d4a508451840cd1686adb7
takasei <takasei1011@gmail.com>
2019-07-23 22:17:25 +0900

    もう一度やってみる

--- 3DGame/EXP.cpp
+++ 3DGame/EXP.cpp
@@ -125,7 +125,7 @@ void UpdateEXP(void)
 		fHeight = 0;
 	}
 
-	//ê›íË
+	//settei
 	SetVertexEXP(0, D3DXVECTOR3(70, 70, 0), D3DXCOLOR(1.0f, 0.27f, 0.0f, 1.0f), MAX_WIDTH, fHeight);
 }
 //=============================================================================
